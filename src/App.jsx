312546d88c0e5b27e9a0f4befbaa7d51d452f17d
f00hy <yaoheng20@gmail.com>
2026-04-03 03:37:44 +0800

    refactor: modify browser tab title and icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import ChatInput from './components/ChatInput';
 import ChatMessages from './components/ChatMessages';
+import robotImage from './assets/robot.png';
 import './App.css';
 
 function App() {
@@ -12,14 +13,22 @@ function App() {
     localStorage.setItem('messages', JSON.stringify(chatMessages));
   }, [chatMessages]);
 
+  const numMessages = Math.ceil(chatMessages.length / 2);
+  const title = `Chatbot${numMessages > 0 ? ` (${numMessages})` : ''}`;
+
   return (
-    <div className="app-container">
-      {chatMessages.length === 0 && (
-        <p className="welcome-message">Welcome to Chatbot! Send a message to get started.</p>
-      )}
-      <ChatMessages chatMessages={chatMessages} />
-      <ChatInput chatMessages={chatMessages} setChatMessages={setChatMessages} />
-    </div>
+    <>
+      <title>{title}</title>
+      <link rel="icon" type="image/png" href={robotImage} />
+
+      <div className="app-container">
+        {chatMessages.length === 0 && (
+          <p className="welcome-message">Welcome to Chatbot! Send a message to get started.</p>
+        )}
+        <ChatMessages chatMessages={chatMessages} />
+        <ChatInput chatMessages={chatMessages} setChatMessages={setChatMessages} />
+      </div>
+    </>
   );
 }
 
