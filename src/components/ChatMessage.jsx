15994aeea49dee9fbe904b68c464915c2e3ffe0a
f00hy <yaoheng20@gmail.com>
2026-04-06 07:44:37 +0800

    feat: add markdown support

--- src/components/ChatMessage.jsx
+++ src/components/ChatMessage.jsx
@@ -1,3 +1,5 @@
+import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
 import robotImage from '../assets/robot.png';
 import userImage from '../assets/user.png';
 
@@ -9,8 +11,13 @@ function ChatMessage({ message, sender, time }) {
         className="w-11 h-11 rounded-full object-cover shrink-0"
         alt={sender}
       />
-      <div className="bg-gray-100 pt-4 pb-3 px-4 rounded-xl mx-3 mb-4 max-w-xs wrap-break-word">
-        {message}
+      <div className="bg-gray-100 pt-4 pb-3 px-4 rounded-xl mx-3 mb-4 max-w-md wrap-break-word">
+        {typeof message === 'string' ? (
+          <ReactMarkdown remarkPlugins={[remarkGfm]}>{message}</ReactMarkdown>
+        ) : (
+          message
+        )}
+        {/* {message} */}
         {time && <div className="text-sm text-gray-500 mt-1 text-right">{time}</div>}
       </div>
     </div>
